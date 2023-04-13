c36e9b37695399b0d0e19195dc8d00b691f53130
jeff3071 <jeff563715@gmail.com>
2023-04-13 12:31:37 +0800

    refactor init

--- MiDaS/run.py
+++ MiDaS/run.py
@@ -69,6 +69,52 @@ def run_depth(img_names, input_path, output_path, model_path, Net, utils, target
     print("finished")
 
 
+def run_depth_1(img, output_path, model_path, Net, utils, target_w=None):
+    """Run MonoDepthNN to compute depth maps.
+
+    Args:
+        input_path (str): path to input folder
+        output_path (str): path to output folder
+        model_path (str): path to saved model
+    """
+    print("initialize")
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    print("device: %s" % device)
+
+    # load network
+    model = Net(model_path)
+    model.to(device)
+    model.eval()
+    
+    # create output folder
+    os.makedirs(output_path, exist_ok=True)
+
+    print("start processing")
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) / 255.0
+    w = img.shape[1]
+    scale = 640. / max(img.shape[0], img.shape[1])
+    target_height, target_width = int(round(img.shape[0] * scale)), int(round(img.shape[1] * scale))
+    img_input = utils.resize_image(img)
+    print(img_input.shape)
+    img_input = img_input.to(device)
+    # compute
+    with torch.no_grad():
+        out = model.forward(img_input)
+    
+    depth = utils.resize_depth(out, target_width, target_height)
+    img = cv2.resize((img * 255).astype(np.uint8), (target_width, target_height), interpolation=cv2.INTER_AREA)
+
+    filename = os.path.join(
+        output_path, 'image'
+    )
+    print(f'write depth file to {filename}')
+    np.save(filename + '.npy', depth)
+    utils.write_depth(filename, depth, bits=2)
+
+    print("finished")
+
 # if __name__ == "__main__":
 #     # set paths
 #     INPUT_PATH = "image"
